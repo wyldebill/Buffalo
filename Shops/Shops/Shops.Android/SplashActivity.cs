7615218d06b9598d4539d9683525e1d6480ac30d
wlliam austin <william.austin@improving.com>
2017-05-27 11:58:37 -0500

    inital commit. add splash screen and start of a loging screen

---
+++ Shops/Shops/Shops.Android/SplashActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.App;
+
+namespace Shops.Droid
+{
+
+    [Activity(Label = "SplashActivity", Icon = "@drawable/icon", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
+        }
+    }
+
+
+    
+}
